aa5550a604ff4aab7ae1eef5baee73c087f47cad
shtwangy <shtwangy@gmail.com>
2021-02-15 18:36:43 +0900

    object lesson 5

--- src/object/alias.ts
+++ src/object/alias.ts
@@ -9,12 +9,59 @@ export default function typeAliasSample() {
     language: 'Japanese',
     name: 'Japan',
   }
-  console.log('Object object sample 5', japan)
+  console.log('Object alias sample 1', japan)
 
   const america: Country = {
     capital: 'Washington D.C.',
     language: 'English',
     name: 'United States of America',
   }
-  console.log('Object object sample 6', america)
+  console.log('Object alias sample 2', america)
+
+  // 合併型(union)と交差型(intersection)
+  type Knight = {
+    hp: number
+    sp: number
+    weapon: string
+    swordSkill: string
+  }
+  type Wizard = {
+    hp: number
+    mp: number
+    weapon: string
+    magicSkill: string
+  }
+
+  // union... Knight or Wizardの型を持つ
+  type Adventurer = Knight | Wizard
+
+  // intersection... Knight & Wizard両方の型を持つ
+  type Paladin = Knight & Wizard
+
+  // Knight寄りの冒険者
+  const adventure1: Adventurer = {
+    hp: 100,
+    sp: 30,
+    weapon: '木の剣',
+    swordSkill: '三連切り',
+  }
+  // Wizard寄りの冒険者
+  const adventure2: Adventurer = {
+    hp: 80,
+    mp: 80,
+    weapon: '木の杖',
+    magicSkill: 'fireball',
+  }
+  console.log('Object alias sample 3', adventure1)
+  console.log('Object alias sample 4', adventure2)
+
+  const paladin: Paladin = {
+    hp: 100,
+    sp: 30,
+    mp: 80,
+    weapon: '銀の剣',
+    swordSkill: '三連切り',
+    magicSkill: 'fireball',
+  }
+  console.log('Object alias sample 5', paladin)
 }
